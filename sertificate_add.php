61a3dd7b41b151404669b0f181f50ac5412f958e
ahal22mail <ahal22mail@gmail.com>
2022-09-07 14:27:26 +0500

    Create sertificate_add.php

---
+++ sertificate_add.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION["UlanyjyAdy"]))
+    {
+        header("Location: index.php");
+        exit();
+    }
+
+    $Sertifikat_Ady="";
+
+    if(isset($_POST["Sertifikat_Ady"]))
+    $Sertifikat_Ady=$_POST["Sertifikat_Ady"];
+
+    if (!empty($_FILES["Sertifikat_Surat"]['name']) && strlen($Sertifikat_Ady)>0) 
+    {
+        $target_file=$Sertifikat_Ady.'.'.pathinfo($_FILES["Sertifikat_Surat"]['name'], PATHINFO_EXTENSION);
+        if(move_uploaded_file($_FILES["Sertifikat_Surat"]["tmp_name"], "assets/sertificates/".$target_file))
+        {
+
+            $data = file_get_contents('language/services.json');
+            $json_arr = json_decode($data, true);
+
+            // add data
+            $json_arr[] = array('name'=>$Sertifikat_Ady, 'type'=>'5', 'image'=>$target_file);
+
+            // encode json and save to file
+            file_put_contents('language/services.json', json_encode($json_arr));
+        }
+    }
+
+  header("Location: sertificates.php");
+  exit();
+?>
