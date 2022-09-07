94e7b76cb525601df493f86ab1993e0474da9c0f
ahal22mail <ahal22mail@gmail.com>
2022-09-07 14:27:23 +0500

    Create sertificate_delete.php

---
+++ sertificate_delete.php
@@ -0,0 +1,42 @@
+<?php
+  session_start();
+
+  if(!isset($_SESSION["UlanyjyAdy"]))
+  {
+      header("Location: index.php");
+      exit();
+  }
+  $id = $_GET['id'];
+
+  $data = file_get_contents('language/services.json');
+  $json_arr = json_decode($data, true);
+  
+  // get array index to delete
+  $arr_index = array();
+  foreach ($json_arr as $key => $value)
+  {
+      if ($key == $id)
+      {
+          $arr_index[] = $key;
+
+          if (unlink('assets/sertificates/'.$value["image"])) {
+            echo ("Sertifikat faýly pozup bolmady");
+            }
+      }
+  }
+  
+  // delete data
+  foreach ($arr_index as $i)
+  {
+      unset($json_arr[$i]);
+  }
+  
+  // rebase array
+  $json_arr = array_values($json_arr);
+  
+  // encode array to json and save to file
+  file_put_contents('language/services.json', json_encode($json_arr));
+
+  header("Location: sertificates.php");
+  exit();
+?>
